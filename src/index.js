77d186649d5f43db261cff909d8f52095e60a2ac
Avidor Turkewitz <aviturkewitz@gmail.com>
2016-12-28 18:06:39 -0500

    Rendering

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux';
+import configureStore from './stores/configureStores'
+import { getShoppingListItems } from './actions/shoppingListItemsActions'
 import App from './App';
 import './index.css';
 
+const store = configureStore();
+
+store.dispatch(getShoppingListItems());
+
 ReactDOM.render(
-   <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
 
